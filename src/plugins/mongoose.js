2082e7afa13023558ff24a9a854bc05bf818ac8b
davidedantonio <davide.dantonio@webeetle.com>
2020-04-15 08:46:48 +0200

    mongoose plugin test

--- src/plugins/mongoose.js
+++ src/plugins/mongoose.js
@@ -1,42 +1,32 @@
 'use strict'
 
 const fp = require('fastify-plugin')
-const mongoose = require('mongoose')
-require('mongoose-schema-jsonschema')(mongoose)
+const mongoosePlugin = require("fastify-mongoose-driver").plugin
+const fs = require('fs')
+const path = require('path')
 
-module.exports = fp(async function (fastify, opts) {
-  class Mongoose {
-    async connection () {
-      if (mongoose.connection.readyState === 0) {
-        await mongoose.connect(process.env.MONGO, {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-          useCreateIndex: true
-        })
-        return true
-      } else {
-        return 'already connected'
-      }
-    }
+const MODELS_DIR = path.join(__dirname, '..', 'models')
 
-    async model (modelName) {
-      await this.connection()
-      const modelSchema = require(`${__dirname}/../models/${modelName}.js`)
-      const model = mongoose.model(modelName, modelSchema)
-      await model.init()
-      await model.ensureIndexes()
-      return model
-    }
+module.exports = fp (async function (fastify, opts) {
+  const dir = fs.readdirSync(MODELS_DIR)
+  const files = []
 
-    close () {
-      if (mongoose.connection.readyState === 1) {
-        mongoose.connection.close()
-        return true
-      } else {
-        return false
-      }
-    }
+  for (let file of dir) {
+    const model = require(`${path.join(MODELS_DIR, file)}`)
+    files.push({
+      name: path.parse(file).name,
+      schema: model
+    })
   }
 
-  fastify.decorate('mongo', new Mongoose())
-}, { name: 'mongoose' })
+  const mongooseOpts = {
+    uri: process.env.MONGO,
+    settings: {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    }
+  }
+  
+  fastify.register(mongoosePlugin, mongooseOpts)
+})
